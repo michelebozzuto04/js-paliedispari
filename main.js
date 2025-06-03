a4a5241f0265758b239903c7f0b344069d4286e6
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-06-03 16:21:31 +0200

    Add functionality for "Pari e Dispari" game with user input and random number generation

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ Palidroma:
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma.
 */
 
+/*
 //Dichiariamo una variabile per memorizzare la parola inserita dall'utente.
 const userString = String(prompt("Inserisci una parola:"));
 
@@ -40,10 +41,8 @@ if (isPalindroma(userString)) {
     console.log("Palindroma");
 } else {
     console.log("Non Palindroma");
-}
-
-
-
+} 
+*/
 
 
 /*Pari e Dispari: 
@@ -53,3 +52,43 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) 
 Dichiariamo chi ha vinto.
 */
+
+// Creiamo una variabile per memorizzare se l'utente ha scelto pari o dispari.
+const userPariDispari = prompt("Scegli pari o dispari:");
+// Creiamo una variabile per memorizzare il numero scelto dall'utente.
+const userNumber = Number(prompt("Scegli un numero da 1 a 5:"));
+
+// Creiamo una funzione che generi un numero random prendendo in entrata due parametri: numero minimo e numero massimo.
+function randomNumber(min, max) {
+    // La funzione ritorna un valore numerico random e arrotondato con Math.
+    number = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log("Numero generato:", number);
+    return number;
+}
+
+// Creiamo una variabile per memorizzare il numero generato per il computer.
+const computerNumber = randomNumber(1, 5);
+
+// Creiamo una funzione per calcolare la somma dei due numeri e controllare se è pari o dispari.
+function sommaPariDispari(num1, num2) {
+    const sum = num1 + num2;
+
+    console.log("Somma:", sum);
+
+    if (sum % 2 === 0) {
+        return "pari";
+    } else {
+        return "dispari"
+    }
+}
+
+// Creiamo una variabile per memorizzare il risultato dato dalla funzione sommaPariDispari.
+const somma = sommaPariDispari(userNumber, computerNumber);
+
+// Mostriamo il risultato all'utente.
+if (userPariDispari === somma) {
+    console.log(somma, "Hai vinto :)");
+} else {
+    console.log(somma, "Hai perso :(");
+
+}
